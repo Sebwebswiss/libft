4ccc0741568962405532ddc8d2d1ba5c82d2c084
sebwebswss <84191341+Sebwebswiss@users.noreply.github.com>
2023-10-09 19:05:31 +0200

    ex lun 09.10

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: sezunec <marvin@42lausanne.ch>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 12:54:40 by sezunec           #+#    #+#             */
-/*   Updated: 2023/10/09 12:54:43 by sezunec          ###   ########.fr       */
+/*   Updated: 2023/10/09 19:00:12 by sezunec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,9 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int	ft_isaplaha(int i);
+int	ft_isalpha(int i);
+int	ft_isdigit(int i);
+int	ft_isalnum(int i);
+int	ft_isascii(int i);
+int	ft_isprint(int i);
 #endif
